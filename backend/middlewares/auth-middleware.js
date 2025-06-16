036cc1953818ea12be1bbdccd085496a5cdd45df
asim <asimneupane11@gmail.com>
2025-06-16 15:46:09 +0545

    feat: add captcha validation middleware and customer authentication middleware for enhanced security

--- backend/middlewares/auth-middleware.js
+++ backend/middlewares/auth-middleware.js
@@ -13,6 +13,7 @@ const { findSingleUserLog } = require("../api/services/session-logs");
 const httpStatus = require("http-status");
 const { sendNotification } = require("../helpers/send-notification");
 const messageConstant = require("../constants/message-constant");
+const { sendMail } = require("../helpers/mailer/mailer");
 const authMiddleware = {};
 
 authMiddleware.authentication = async (req, res, next) => {
@@ -193,28 +194,56 @@ authMiddleware.authorization = async (req, res, next) => {
 
         if (result) {
           if (!isSuperAdmin) {
-            const user = await userModel.findByPk(result.userId);
+            const user = await userModel.findByPk(result.userId, {
+              include: {
+                model: userModel,
+                as: "supervisor",
+              },
+            });
             if (!user) {
-              return {
-                status: 404,
-                success: false,
-                message: "User not found",
-              };
+              return responseHelper.sendResponse(
+                res,
+                HttpStatus.NOT_FOUND,
+                false,
+                null,
+                null,
+                "User Not Found",
+                null,
+              );
             }
             await sendNotification(
               user.username,
-              user.supervisorId,
-              user.id,
+              user?.supervisorId,
+              user?.id,
               EN.APPROVAL_REQUEST,
               "Approval_Request",
               result?.id,
             );
+            //email notification
+            const placeholders = {
+              name: user?.supervisor?.username,
+              senderUserName: user.username,
+              requestId: result.id.toString(),
+              email: user.email,
+              supervisorId: user.supervisorId,
+            };
+
+            //    desc:"This template will be use when user send approval request for new changes",
+            sendMail(
+              "requestForApproval",
+              placeholders,
+              user?.supervisor?.email,
+            ).catch((error) => {
+              // Log the error without throwing it further
+              console.error("Mail sending error:", error);
+            });
           }
           return responseHelper.sendResponse(
             res,
             200,
             true,
             result,
+            null,
             isSuperAdmin
               ? "Action approved directly"
               : "Request sent for approval",
